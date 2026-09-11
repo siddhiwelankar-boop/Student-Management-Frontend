7e88148f7fdc900fb5b6baab25e151f9f898477f
Siddhi Welankar <siddhiwelankar@email.com>
2026-09-11 15:15:08 +0530

    Connect frontend with backend

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // FastAPI URL
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://student-management-backend-v4v1.onrender.com";
 
 
 // ============================================================
